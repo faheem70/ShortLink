1bf756edfb6ade0dbbf8b6b0ec633af6add5d3d3
Faheem Akhtar <faheemakhtar19730@gmail.com>
2023-12-13 00:04:07 +0530

    update

--- backend/Database/database.js
+++ backend/Database/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const url = "mongodb://localhost:27017/yourDatabaseName"
+const url = "mongodb://localhost:27017/ShortLink"
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
