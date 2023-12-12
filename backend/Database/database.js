81245318fc058005b38837bca254a820a6dd5ab9
Faheem Akhtar <faheemakhtar19730@gmail.com>
2023-12-13 00:26:51 +0530

    update

--- backend/Database/database.js
+++ backend/Database/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const url = "mongodb+srv://shortLink1:<password>@cluster0.uihjk1o.mongodb.net/?retryWrites=true&w=majority"
+const url = "mongodb+srv://shortLink1:ShortLink123@cluster0.uihjk1o.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
